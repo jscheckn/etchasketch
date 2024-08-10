c996aef137a36b3e629a6ffd37a0703cdbbc9f39
jscheckn <schecknerjm@yahoo.com>
2024-08-10 09:22:04 -0400

    working sketch only with 16x16

--- script.js
+++ script.js
@@ -3,13 +3,18 @@ function makeDivs(){
     for(let i = 0; i < 256; i++){
         let square = document.createElement('div');
         container.appendChild(square);
+        square.classList.add("square");
+        
         
     }
     
     
 
 }
-
 makeDivs();
+document.querySelector('#container').addEventListener('mouseover', (e) => {
+    let t = e.target;
+    t.classList.add("hovered");
+})
 
 
